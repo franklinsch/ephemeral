252bf06628bee163b963ea21f5011504c28e10d0
DANIEL GRUMBERG <dany.grumberg@gmail.com>
2017-02-04 20:46:03 +0000

    Add server project

---
+++ server/Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "Ephemeral",
+    dependencies: [
+        .Package(
+        url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+        majorVersion: 2, minor: 0
+        )
+    ]
+)
